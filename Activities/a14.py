2be976174f9623ed7efbfc7ca1e21caac4eeac9e
K-cube <kcubeix@gmail.com>
2022-11-03 20:31:38 +0630

    Added comments on a14

--- Activities/a14.py
+++ Activities/a14.py
@@ -1,9 +1,12 @@
+# Get first name and last name
 first_name = input("Enter the first name: ")
 last_name = input("Enter the last name: ")
 
+# Modify last name
 if len(last_name) < 4:
     last_name += "X" * (4-len(last_name))
 else:
     last_name = last_name[:4]
 
+# Print username
 print(first_name[0] + last_name)
