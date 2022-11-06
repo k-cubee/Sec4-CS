eec43e1db77a30a9faeab42944f2bd1c9892eb6a
K-cube <kcubeix@gmail.com>
2022-11-06 17:23:43 +0630

    Did a bit of leetcode

---
+++ Tests/2pointers.py
@@ -0,0 +1,19 @@
+def reverseVowels(s):
+    vowels = set(list("aeiouAEIOU"))
+    s = list(s)
+    ptr_1 = 0
+    ptr_2 = len(s) - 1
+    while ptr_1 < ptr_2:
+        if s[ptr_1] in vowels and s[ptr_2] in vowels:
+            s[ptr_1] = s[ptr_2]
+            s[ptr_2] = s[ptr_2]
+            ptr_1 += 1
+            ptr_2 -= 1
+        if s[ptr_1] not in vowels:
+            ptr_1 += 1
+        if s[ptr_2] not in vowels:
+            ptr_2 -= 1
+    return ''.join(s)
+
+
+print(reverseVowels("Hello World"))
